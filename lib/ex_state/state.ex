93df6a9b1b2d33a2c9397cf8156d41b5e7898382
Jindrich K. Smitka <jsmitka@smita.info>
2020-03-28 17:03:06 +0100

    Added base parsing

--- lib/ex_state/state.ex
+++ lib/ex_state/state.ex
@@ -3,43 +3,9 @@ defmodule ExState.State do
   A FSM state
   """
 
-  @type type() :: :atomic | :compound | :parallel | :final | :history
+  defstruct value: nil
 
-  defmodule Atomic do
-    @moduledoc """
-    An atomic state node
-    """
-    defstruct on: %{}
-  end
-
-  defmodule Compound do
-    @moduledoc """
-    A compound state node
-    """
-    defstruct initial: nil,
-              on: %{},
-              states: %{}
-  end
-
-  defmodule Parallel do
-    @moduledoc """
-    A parallel state node
-    """
-    defstruct on: %{},
-              states: %{}
-  end
-
-  defmodule Final do
-    @moduledoc """
-    A final state node
-    """
-    defstruct []
-  end
-
-  defmodule History do
-    @moduledoc """
-    A history state node
-    """
-    defstruct []
+  def create(state) do
+    %__MODULE__{value: state}
   end
 end
