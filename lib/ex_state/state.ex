c31afeb3825e9cc5381d77f7e496b102e433b577
Jindrich K. Smitka <jsmitka@smita.info>
2020-04-07 10:28:50 +0200

    Refactored transition request

--- lib/ex_state/state.ex
+++ lib/ex_state/state.ex
@@ -3,11 +3,25 @@ defmodule ExState.State do
   A FSM state
   """
 
+  alias ExState.{Request, Transition}
+
   defstruct value: nil,
             event: nil,
             context: nil
 
   def create(state, context, event) do
     %__MODULE__{value: state, event: event, context: context}
+  end
+
+  def value(%__MODULE__{value: value}), do: value
+
+  def context(%__MODULE__{context: context}), do: context
+
+  def move(%Request{transition: transition, event: event, context: context}) do
+    %__MODULE__{
+      value: Transition.target(transition),
+      event: event,
+      context: context
+    }
   end
 end
