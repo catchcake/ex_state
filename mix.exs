df1c333b5607c184ac00b0c4fe0ecccc5d324377
Jindrich K. Smitka <jsmitka@smita.info>
2020-04-08 17:27:31 +0200

    Bump version: 1.0.1 → 1.0.2

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule ExState.MixProject do
           :no_opaque
         ]
       ],
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
