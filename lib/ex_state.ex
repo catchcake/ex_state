b5d3e904540565f5f5d5110f52baf677b60d65b9
Jindrich K. Smitka <jsmitka@smita.info>
2020-05-31 20:37:09 +0200

    Added main module delegates

--- lib/ex_state.ex
+++ lib/ex_state.ex
@@ -1,3 +1,13 @@
 defmodule ExState do
   @moduledoc false
+
+  @doc """
+  See `ExState.Machine.create/2`
+  """
+  defdelegate create(machine, opts \\ %{}), to: ExState.Machine
+
+  @doc """
+  See `ExState.Machine.transition/2`
+  """
+  defdelegate transition(machine, event), to: ExState.Machine
 end
