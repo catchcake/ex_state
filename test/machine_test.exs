c5890db35fe5ffc930ef78b1ae5a50dd7fbcad27
Jindrich K. Smitka <jsmitka@smita.info>
2020-05-31 20:42:22 +0200

    Refactored example definition as comment

--- test/machine_test.exs
+++ test/machine_test.exs
@@ -5,74 +5,72 @@ defmodule MachineTest do
 
   alias ExState.{Event, Machine, Context}
 
-  @definition """
-  %{
-    id: "fetch",
-    initial: :idle,
-    states: %{
-      idle: %{
-        type: :atomic,
-        on: %{
-          FETCH: :pending
-        }
-      },
-      pending: %{
-        type: :parallel,
-        states: %{
-          resource1: %{
-            type: :compound,
-            initial: :pending,
-            states: %{
-              pending: %{
-                on: %{
-                  "FULFILL.resource1" => :success
-                }
-              },
-              success: %{
-                type: :final
-              }
-            }
-          },
-          resource2: %{
-            type: :compound,
-            initial: :pending,
-            states: %{
-              pending: %{
-                on: %{
-                  "FULFILL.resource2" => :success
-                }
-              },
-              success: %{
-                type: :final
-              }
-            }
-          }
-        },
-        onDone: :success
-      },
-      success: %{
-        type: :compound,
-        initial: :items,
-        states: %{
-          items: %{
-            on: %{
-              "ITEM.CLICK" => :item
-            }
-          },
-          item: %{
-            on: %{
-              BACK: :items
-            }
-          },
-          hist: %{
-            type: :history,
-            history: :shallow
-          }
-        }
-      }
-    }
-  }
-  """
+  # %{
+  #   id: "fetch",
+  #   initial: :idle,
+  #   states: %{
+  #     idle: %{
+  #       type: :atomic,
+  #       on: %{
+  #         FETCH: :pending
+  #       }
+  #     },
+  #     pending: %{
+  #       type: :parallel,
+  #       states: %{
+  #         resource1: %{
+  #           type: :compound,
+  #           initial: :pending,
+  #           states: %{
+  #             pending: %{
+  #               on: %{
+  #                 "FULFILL.resource1" => :success
+  #               }
+  #             },
+  #             success: %{
+  #               type: :final
+  #             }
+  #           }
+  #         },
+  #         resource2: %{
+  #           type: :compound,
+  #           initial: :pending,
+  #           states: %{
+  #             pending: %{
+  #               on: %{
+  #                 "FULFILL.resource2" => :success
+  #               }
+  #             },
+  #             success: %{
+  #               type: :final
+  #             }
+  #           }
+  #         }
+  #       },
+  #       onDone: :success
+  #     },
+  #     success: %{
+  #       type: :compound,
+  #       initial: :items,
+  #       states: %{
+  #         items: %{
+  #           on: %{
+  #             "ITEM.CLICK" => :item
+  #           }
+  #         },
+  #         item: %{
+  #           on: %{
+  #             BACK: :items
+  #           }
+  #         },
+  #         hist: %{
+  #           type: :history,
+  #           history: :shallow
+  #         }
+  #       }
+  #     }
+  #   }
+  # }
 
   test "should create move machine to initial state" do
     definition = %{
